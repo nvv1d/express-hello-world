2e828e33595098138ce5c25545adabe7160a2eb1
nvv1d <blxxdrxvxn@navit.cloudns.ph>
2023-04-21 01:55:57 +0330

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ var exec = require("child_process").exec;
 const os = require("os");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 var request = require("request");
-//const fetch = require("node-fetch");
+const fetch = require("node-fetch");
 const render_app_url = "https://clever-mite-sneakers.cyclic.app";
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+res.send("hello world");
 });
 
 app.get("/status", (req, res) => {
@@ -70,76 +70,23 @@ app.use(
     changeOrigin: true, // 默认false，是否需要改变原始主机头为目标URL
     ws: true, // 是否代理websockets
     pathRewrite: {
-      "^/": "/",
+      "^/": "/", // 重写路径
     },
-    onProxyReq: function onProxyReq(proxyReq, req, res) {
-      //console.log("-->  ",req.method,req.baseUrl,"->",proxyReq.host + proxyReq.path);
+    onProxyReq: (proxyReq, req, res) => {
+      // 可选：设置自定义请求头
+      proxyReq.setHeader("X-Special-Proxy-Header", "foobar");
     },
   })
 );
 
-/* keepalive  begin */
-function keepalive() {
-  // 1.请求主页，保持唤醒
-  request(render_app_url, function (error, response, body) {
-    if (!error) {
-      console.log("主页发包成功！");
-      console.log("响应报文:", body);
-    } else console.log("请求错误: " + error);
-  });
-
-  //2. 本地进程检测,保活web.js
-  exec("ps -ef", function (err, stdout, stderr) {
-    if (err) {
-      console.log("保活web.js-本地进程检测-命令行执行失败:" + err);
+setInterval(() => {
+  request.get("http://localhost:" + port + "/", (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      console.log(new Date() + " 保持应用在线状态成功");
     } else {
-      if (stdout.includes("./web.js -c ./config.json"))
-        console.log("保活web.js-本地进程检测-web.js正在运行");
-      //命令调起web.js
-      else startWeb();
-      if (stdout.includes("/bin/bash nezha.sh"))
-        console.log("保活哪吒-本地进程检测-哪吒正在运行")
-      else startNezha();
+      console.log(new Date() + " 保持应用在线状态失败");
     }
   });
-}
-
-//保活频率设置为30秒
-setInterval(keepalive, 30 * 1000);
-/* keepalive  end */
-
-function startWeb() {
-  let startWebCMD = "chmod +x ./web.js && ./web.js -c ./config.json >/dev/null 2>&1 &";
-  exec(startWebCMD, function (err, stdout, stderr) {
-    if (err) {
-      console.log("启动web.js-失败:" + err);
-    } else {
-      console.log("启动web.js-成功!");
-    }
-  });
-}
-
-function startNezha() {
-  let startNezhaCMD = "/bin/bash nezha.sh server.abc.tk 5555 dfzPfEOagGDCAVhM4s >/dev/null 2>&1 &";
-  exec(startNezhaCMD, function (err, stdout, stderr) {
-    if (err) {
-      console.log("启动哪吒-失败:" + err);
-    } else {
-      console.log("启动哪吒-成功!");
-    }
-  });
-}
-
-/* init  begin */
-exec("tar -zxvf src.tar.gz", function (err, stdout, stderr) {
-  if (err) {
-    console.log("初始化-解压资源文件src.tar.gz-失败:" + err);
-  } else {
-    console.log("初始化-解压资源文件src.tar.gz-成功!");
-    startWeb();
-    startNezha();
-  }
-});
-/* init  end */
+}, 1000 * 60 * 5);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
